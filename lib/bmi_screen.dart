936171c11bc4b2506113965cefe2a5001244c0fc
abdi <40822571+king1254@users.noreply.github.com>
2021-09-18 18:33:02 +0300

    silder

--- lib/bmi_screen.dart
+++ lib/bmi_screen.dart
@@ -15,71 +15,108 @@ class _BmiScreenState extends State<BmiScreen> {
       body: Column(
         children: [
           Expanded(
-            child: Row(
-              children: [
-                
-            Expanded(
-              child:Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        10.0,
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Container(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            10.0,
+                          ),
+                          color: Colors.grey,
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.ac_unit,
+                              size: 70.0,
+                            ),
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'male',
+                              style: TextStyle(fontSize: 25.0),
+                            )
+                          ],
+                        ),
                       ),
-                      color: Colors.grey,
-                    ),
-                    child: Column(
-                        mainAxisAlignment : MainAxisAlignment.center,
-
-                      children: [
-                        Icon(
-                          Icons.ac_unit,
-                          size: 70.0,
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ),
-                        Text(
-                          'male',
-                          style: TextStyle(fontSize: 25.0),
-                        )
-                      ],
                     ),
                   ),
-                ),),
-                  Expanded(child:Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(
-                        10.0,
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                    child: Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                        color: Colors.grey,
                       ),
-                      color: Colors.grey,
-                    ),
-                    child: Column(
-                        mainAxisAlignment : MainAxisAlignment.center,
-
-                      children: [
-                        Icon(
-                          Icons.ac_unit,
-                          size: 70.0,
-                        ),
-                        SizedBox(
-                          height: 15.0,
-                        ),
-                        Text(
-                          'male',
-                          style: TextStyle(fontSize: 25.0),
-                        )
-                      ],
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.ac_unit,
+                            size: 70.0,
+                          ),
+                          SizedBox(
+                            height: 15.0,
+                          ),
+                          Text(
+                            'male',
+                            style: TextStyle(fontSize: 25.0),
+                          )
+                        ],
+                      ),
                     ),
                   ),
-                ),),
-              ],
+                ],
+              ),
             ),
           ),
 
+///////////////////////////////
+          ///
+          ///
+          ///
+          ///
+          ///
 
+          Expanded(
+            child: Column(
+              children: [
+                Text(
+                  'ارتفاع',
+                  style: TextStyle(fontSize: 25.0),
+                ),
+                SizedBox(
+                  height: 15.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.baseline,
+                  textBaseline: TextBaseline.alphabetic,
+                  children: [
+                    Text(
+                      '180',
+                      style: TextStyle(fontSize: 25.0),
+                    ),
+                    Text(
+                      'cm',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                  ],
+                )
+              ],
+            ),
+          ),
           ///////////////////////////////////
           Expanded(
               child: Container(
@@ -87,11 +124,7 @@ class _BmiScreenState extends State<BmiScreen> {
             color: Colors.green,
           )),
           //////////////////////////////////
-          Expanded(
-              child: Container(
-            width: double.infinity,
-            color: Colors.yellow,
-          )),
+
           Container(
             width: double.infinity,
             color: Colors.red,
