e1d5015647ba42d7b55c9577396a15960ead3938
abdi <40822571+king1254@users.noreply.github.com>
2021-09-20 13:21:45 +0300

    silder& and 2 button

--- lib/bmi_screen.dart
+++ lib/bmi_screen.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 
+// ignore: must_be_immutable
 class BmiScreen extends StatefulWidget {
+
   @override
   _BmiScreenState createState() => _BmiScreenState();
 }
 
 class _BmiScreenState extends State<BmiScreen> {
+  bool isMale = true;
+  double height = 120.0;
+
+  int weight = 40;
+  int age = 20;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,32 +28,37 @@ class _BmiScreenState extends State<BmiScreen> {
               child: Row(
                 children: [
                   Expanded(
-                    child: Container(
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isMale = false;
+                        });
+                      },
                       child: Container(
-                        decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            10.0,
+                        child: Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(
+                              10.0,
+                            ),
+                            color: !isMale ? Colors.blue : Colors.grey[400],
                           ),
-                          color: Colors.grey,
-                        ),
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-                      
-
-                               Image(
-                              image: AssetImage('assets/images/female.png'),
-                              height: 90.0,
-                              width: 90.0,
-                            ),
-                            SizedBox(
-                              height: 15.0,
-                            ),
-                            Text(
-                              'أنثى',
-                              style: TextStyle(fontSize: 25.0),
-                            )
-                          ],
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Image(
+                                image: AssetImage('assets/images/female.png'),
+                                height: 90.0,
+                                width: 90.0,
+                              ),
+                              SizedBox(
+                                height: 15.0,
+                              ),
+                              Text(
+                                'أنثى',
+                                style: TextStyle(fontSize: 25.0),
+                              )
+                            ],
+                          ),
                         ),
                       ),
                     ),
@@ -54,29 +67,36 @@ class _BmiScreenState extends State<BmiScreen> {
                     width: 20.0,
                   ),
                   Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(
-                          10.0,
+                    child: GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          isMale = true;
+                        });
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(
+                            10.0,
+                          ),
+                          color: isMale ? Colors.blue : Colors.grey[400],
                         ),
-                        color: Colors.grey,
-                      ),
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Image(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Image(
                               image: AssetImage('assets/images/male.png'),
                               height: 90.0,
                               width: 90.0,
                             ),
-                          SizedBox(
-                            height: 15.0,
-                          ),
-                          Text(
-                            'ذكر',
-                            style: TextStyle(fontSize: 25.0),
-                          )
-                        ],
+                            SizedBox(
+                              height: 15.0,
+                            ),
+                            Text(
+                              'ذكر',
+                              style: TextStyle(fontSize: 25.0),
+                            )
+                          ],
+                        ),
                       ),
                     ),
                   ),
@@ -93,165 +113,197 @@ class _BmiScreenState extends State<BmiScreen> {
           ///
 
           Expanded(
-            
             child: Padding(
-              padding: const EdgeInsets.all(20.0),
+              padding: const EdgeInsets.symmetric(
+                horizontal: 20.0,
+              ),
               child: Container(
-                   decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(
-                            10.0,
-                          ),
-                          color: Colors.grey,
-                        ),
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.center,
                   children: [
                     Text(
                       'ارتفاع',
-                      style: TextStyle(fontSize: 25.0),
-                    ),
-                    SizedBox(
-                      height: 15.0,
+                      style: TextStyle(
+                        fontSize: 25.0,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.baseline,
+                      mainAxisAlignment: MainAxisAlignment.center,
                       textBaseline: TextBaseline.alphabetic,
                       children: [
                         Text(
-                          '180',
-                          style: TextStyle(fontSize: 25.0),
+                          '${height.round()}',
+                          style: TextStyle(
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.w900,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
                         ),
                         Text(
-                          'سنتيمتر',
+                          'سنتمتر',
                           style: TextStyle(
-                              fontWeight: FontWeight.bold, fontSize: 20.0),
+                            fontSize: 20.0,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
                       ],
                     ),
-       Slider(
-         value: 120.0, 
-    max: 220.0,
-    min: 80.0,
-         onChanged: (value) {
-         print(value.round());}),
+                    Slider(
+                      value: height,
+                      max: 220.0,
+                      min: 80.0,
+                      onChanged: (value) {
+                        setState(() {
+                          height = value;
+                        });
+                      },
+                    ),
                   ],
+                ),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    10.0,
+                  ),
+                  color: Colors.grey[400],
                 ),
               ),
             ),
           ),
-          
+
           ///////////////////////////////////
           Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Row(
-                  children: [
-
-                            Expanded( 
-                              child:Container(
-                      
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                              10.0,
-                            ),
-                            color: Colors.grey,
-                          ),
+            child: Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                children:
+                [
+                  Expanded(
+                    child: Container(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-                Text(
-                          'ارتفاع',
-                          style: TextStyle(fontSize: 25.0),
-                        ),
-                           Text(
-                          '180',
-                          style: TextStyle(fontSize: 25.0),
-                        ),
-                        Row(
-                                            mainAxisAlignment: MainAxisAlignment.center,
-
-                          children: [
-                            FloatingActionButton(
-                              onPressed:() {},
-                              mini: true,
-                              child: Icon(
-                           
-
-                                Icons.remove,
-                              ),
-                              
-                              
-                              ),
-                                  SizedBox(width: 20.0,),
-                                     FloatingActionButton(
-                              onPressed:() {},
-                                                        mini: true,
-
-                              child: Icon(
-                                Icons.add,
-                              ),
-                               ),
-                            ],)
-        ],
-          ),
-                    ),
-                     ),
-                     SizedBox(
-                       width: 20.0,
-                     ),
-                     Expanded( 
-                              child:Container(
-                      
-                          decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(
-                              10.0,
+                        children: [
+                          Text(
+                            'WEIGHT',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
                             ),
-                            color: Colors.grey,
                           ),
+                          Text(
+                            '$weight',
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    weight--;
+                                  });
+                                },
+                                heroTag: 'weight-',
+                                mini: true,
+                                child: Icon(
+                                  Icons.remove,
+                                ),
+                              ),
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    weight++;
+                                  });
+                                },
+                                heroTag: 'weight+',
+                                mini: true,
+                                child: Icon(
+                                  Icons.add,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          10.0,
+                        ),
+                        color: Colors.grey[400],
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 20.0,
+                  ),
+                  Expanded(
+                    child: Container(
                       child: Column(
                         mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-                Text(
-                          'ارتفاع',
-                          style: TextStyle(fontSize: 25.0),
+                        children: [
+                          Text(
+                            'AGE',
+                            style: TextStyle(
+                              fontSize: 25.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          Text(
+                            '$age',
+                            style: TextStyle(
+                              fontSize: 40.0,
+                              fontWeight: FontWeight.w900,
+                            ),
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    age--;
+                                  });
+                                },
+                                heroTag: 'age-',
+                                mini: true,
+                                child: Icon(
+                                  Icons.remove,
+                                ),
+                              ),
+                              FloatingActionButton(
+                                onPressed: () {
+                                  setState(() {
+                                    age++;
+                                  });
+                                },
+                                heroTag: 'age+',
+                                mini: true,
+                                child: Icon(
+                                  Icons.add,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(
+                          10.0,
                         ),
-                           Text(
-                          '180',
-                          style: TextStyle(fontSize: 25.0),
-                        ),
-                        Row(
-                                            mainAxisAlignment: MainAxisAlignment.center,
-
-                          children: [
-                            FloatingActionButton(
-                              onPressed:() {},
-                              mini: true,
-                              child: Icon(
-                           
-
-                                Icons.remove,
-                              ),
-                              
-                              
-                              ),
-                              SizedBox(width: 20.0,),
-                                     FloatingActionButton(
-                              onPressed:() {},
-                                                        mini: true,
-
-                              child: Icon(
-                                Icons.add,
-                              ),
-                               ),
-                            ],)
-        ],
-          ),
+                        color: Colors.grey[400],
+                      ),
                     ),
-                     ),
-                  ],
-                ),
-              )),
+                  ),
+                ],
+              ),
+            ),
+          ),
 
           //////////////////////////////////
 
@@ -260,7 +312,7 @@ class _BmiScreenState extends State<BmiScreen> {
             color: Colors.red,
             child: MaterialButton(
               onPressed: () {},
-              child: Text("text me"),
+              child: Text(" اضغظ هنا"),
             ),
           ),
         ],
